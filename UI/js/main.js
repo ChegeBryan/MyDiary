0c2e6fd2c70ac69aa5daeeab4b3e7c1e76a73b9f
cBryan <cbbryan8@gmail.com>
2018-07-12 15:30:48 +0300

    [Feature-158937106] Created responsive landing page

---
+++ UI/js/main.js
@@ -0,0 +1,9 @@
+function showPassword() {
+    var x = document.getElementById("password");
+    x.type = "text";
+}
+function hidePassword() {
+    var x = document.getElementById("password");
+    x.type = 'password';
+}
+
